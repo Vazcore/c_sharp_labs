037688ef9c761d658644abd565d7568a70d85bfb
Vazcore <vazcore@live.ru>
2016-02-23 20:36:11 +0300

    Labs 6-7

---
+++ Lab7_ind/Lab7_ind/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Lab7_ind
+{
+    class Printer {
+
+        public void printList(String[] list)
+        {
+            //Console.WriteLine(list.Length);
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.WriteLine("Tag #" + (i+1) + ": " + list[i]);                
+            }
+        }
+    }
+
+
+
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            // helper object for printing
+            Printer printer = new Printer();           
+
+            // read from file
+            try 
+            {
+                using (StreamReader sr = new StreamReader("input.html"))
+                { 
+                    // read whole data
+                    String data = sr.ReadToEnd();                    
+
+                    Regex regex = new Regex(@"<\s*\w.*?>|(<\s*\/\s*\w\s*.*?>|<\s*br\s*>)");
+                    Match match = regex.Match(data);
+
+                    StreamWriter sw = new StreamWriter("out.txt", false, Encoding.GetEncoding(1251));
+
+                    while (match.Success)
+                    {
+                        Console.WriteLine(match.Groups[0].Value);
+                        sw.WriteLine(match.Groups[0].Value);
+                        match = match.NextMatch();
+                    }
+                    
+                    
+                }
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine("Error while reading from file");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab7_ind/Lab7_ind/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Lab7_ind")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab7_ind")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c1e3a8ac-8656-41e1-9d41-29857aa0d245")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
